a65880cfae5780b9d0ef0acdae00bef789596469
Nguyễn Việt Huy <huynvse173703@fpt.edu.vn>
2026-03-03 00:56:20 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './Homepage';
 import Layout from './Layout';
-
+import Login from './Login';
 
 
 function App() {
@@ -9,7 +9,8 @@ function App() {
     <Layout>
       <Routes>
         <Route path="/" element={<HomePage />} />
-
+        <Route path="/login" element={<Login />} />
+        
       </Routes>
     </Layout>
 
